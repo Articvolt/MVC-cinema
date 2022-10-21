1e7ad84b7603edf0e2fe16bbd353e5b2dc8bcf3c
UgoDASILVA <ugodasilva67@gmail.com>
2022-10-21 10:50:57 +0200

    add files MVC

---
+++ controller/CinemaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Controller;
+use Model\Connect;
+
+class CinemaController {
+
+
+
+    public function listFilms() {
+
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+            SELECT titre, annee_sortie
+            FROM film
+        ");
+
+        require "view/listFilms.php";
+    }
+}
--- index.php
+++ index.php
@@ -7,3 +7,12 @@ spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });
 
+$ctrlCinema = new CinemaController();
+
+if(isset($_GET["action"])) {
+    switch ($_GET["action"]) {
+
+        case "listFilms" : $ctrlCinema->listFilms(); break;
+        case "listActeurs" : $ctrlCinema->listActeurs(); break;
+    }
+}
